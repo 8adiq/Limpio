12c21f21e2d1d549838de093365f1ab4ce6bf586
8adiq <abdul.latif708@yahoo.com>
2024-06-02 14:43:42 +0000

    connection to temporal folder made

--- cleaner.py
+++ cleaner.py
@@ -10,7 +10,7 @@ from watchdog.events import FileSystemEventHandler
 download_folder_path = 'C:/Users/sadiq/Downloads'
 dest_folder_path = "C:/filesFromDownloads"
 
-def folder_scanner(path=Path()):
+def folder_organizer(path=Path()):
     for entry in path.iterdir():
         if entry.is_file():
             full_name = os.path.basename(entry)
@@ -21,49 +21,17 @@ def folder_scanner(path=Path()):
             extension_folder_path = os.path.join(dest_folder_path,extension.lstrip('.')) 
             os.makedirs(extension_folder_path,exist_ok=True) # creating folders based on extensions
 
+            final_file_path = os.path.join(extension_folder_path, filename + extension)
+            shutil.move(entry,final_file_path)
+            print(f'filename {filename} has been copied to {dest_folder_path}')
+
 
         elif entry.is_dir():
-            folder_scanner(entry)
+            folder_organizer(entry)
 
 
+folder_organizer(path=Path(download_folder_path))
 
-# def transfer_files(filename,extension):
-#     ...
-#     extension_folder = os.path.join(dest_folder_path,extension.lstrip('.'))
-#     print(extension_folder)
-
-
-folder_scanner(path=Path(download_folder_path))
-# transfer_files(filename,extension)
-
-
-    
-
-
-
-
-# class DownloadOrganizer(FileSystemEventHandler):
-#     ...
-
-#     def __init__(self,dest_folder):
-#         self.dest_folder = dest_folder
-
-#     def scan(self):
-
-#         source_path = ''
-        
-#         # spliting into filename and extension
-#         filename, extension = os.path.splitext(source_path)
-
-#         # Creating subfolders path for a particular file type 
-#         extension_folder = os.path.join(self.dest_folder,extension.lstrip('.'))
-
-#         # creating the subfolders if it does not exits
-#         os.makedirs(extension_folder,exit_ok= True)
-
-#         dest_path = os.path.join(self.dest_folder, filename + extension)
-#         shutil.copy2(source_path,dest_path)
-#         print(f" Copied {filename}{extension} to {extension_folder}")
         
 
 def main():
